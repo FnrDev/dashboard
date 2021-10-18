610ba87ca25d262dcb182b6af045a8e7323b28ca
FnrDev <ah2014bh@outlook.com>
2021-10-18 13:10:46 +0300

    no need

--- router/support.js
+++ router/support.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    let client = req.bot
     res.redirect(`https://discord.gg/${process.env.SUPPORT}`)
 })
 
