ff84cf428a5017a2e0d4b45e12ac2e5f9224f952
FnrDev <ah2014bh@outlook.com>
2021-10-18 14:02:48 +0300

    fetch application to get "about me"

--- router/index.js
+++ router/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.render('index')
+    const client = req.bot;
+    const fetchApplication = await client.application.fetch();
+    res.render('pages/index', {
+        description: fetchApplication.description || `I can\'t find about me in ${client.user.username} bot`
+    })
 })
 
 module.exports = router;
